8939342ccfd2c9fb196089ed214a61e29b1a016f
Baptiste Fourmont <baptiste_fourmont@tutanota.com>
2022-04-28 12:09:40 +0200

    [ADD] Quelques contraintes

--- TABLES/create.sql
+++ TABLES/create.sql
@@ -2,7 +2,6 @@ DROP DATABASE IF EXISTS projet_42;
 CREATE DATABASE projet_42;
 -- suppression des tables précédentes
 
-DROP TYPE IF EXISTS type_navire;
 DROP TABLE IF EXISTS nation;
 DROP TABLE IF EXISTS navire;
 DROP TABLE IF EXISTS port;
@@ -17,35 +16,36 @@ DROP TABLE IF EXISTS corresponding;
 DROP TABLE IF EXISTS diplomatics_relations;
 
 CREATE TYPE type_navire AS ENUM('Flûte', 'Galion', 'Gabare');
+CREATE TYPE type_voyage AS ENUM('intercontinental', 'continental');
 
 -- creation des tables
 CREATE TABLE nation (
-    name varchar(32) NOT NULL,
+    name varchar(32) NOT NULL ON UPDATE CASCADE,
     PRIMARY KEY (nom),
     CONSTRAINT CHK_Nation CHECK(name NOT LIKE '%[^A-Z]%') 
 );
 
 CREATE TABLE port(
-    name varchar(32) NOT NULL,
-    category int NOT NULL,
+    name varchar(32) NOT NULL ON UPDATE CASCADE,
+    category int NOT NULL ON UPDATE CASCADE,
     PRIMARY KEY (nom),
-    CONSTRAINT CHK_Port CHECK(name NOT LIKE '%[^A-Z]%')
+    CONSTRAINT CHK_Port CHECK(name NOT LIKE '%[^A-Z]%' AND category >= 1 AND category <= 5)
 );
 
 CREATE TABLE navire(
     id_navire SERIAL NOT NULL,
     type type_navire NOT NULL,
     category int NOT NULL, 
-    passagers_max int NOT NULL,
+    passagers_max int NOT NULL DEFAULT 0,
     crew int NOT NULL,
-    cale_max int NOT NULL,
+    cale_max int NOT NULL DEFAULT 0,
     PRIMARY KEY(id_navire),
-    CONSTRAINT CHK_Navire CHECK (category >= 1 AND category <= 5 and passagers_max > crew)
+    CONSTRAINT CHK_Navire CHECK (crew > 0 AND passagers_max >= 0 AND category >= 1 AND category <= 5 and passagers_max > crew)
 );
 
 CREATE TABLE voyage(
     begin_date date NOT NULL DEFAULT current_date,
-    type varchar(32) NOT NULL,
+    type type_voyage NOT NULL,
     duration int NOT NULL,
     passagers int NOT NULL,
     cale int NOT NULL,
@@ -66,12 +66,12 @@ CREATE TABLE produits(
     product_id SERIAL NOT NULL,
     name varchar(32) NOT NULL,
     perishable boolean NOT NULL,
-    quantity int NOT NULL,
-    volume int NOT NULL,
-    shelf_life int NOT NULL,
-    value_per_kilo int NOT NULL,
+    quantity int NOT NULL DEFAULT 1,
+    volume int NOT NULL DEFAULT 1,
+    shelf_life int NOT NULL DEFAULT 1,
+    value_per_kilo int NOT NULL DEFAULT 1,
     PRIMARY KEY(product_id),
-    CONSTRAINT CHK_produits CHECK (quantity > 0 && shelf_life > 1 && value_per_kilo > 0),
+    CONSTRAINT CHK_produits CHECK (volume > 0 AND quantity > 0 && shelf_life > 0 && value_per_kilo > 0),
     CONSTRAINT CHK_Name CHECK(name NOT LIKE '%[^A-Z]%')
 );
 
@@ -96,15 +96,15 @@ CREATE TABLE nationality(
     id_navire SERIAL NOT NULL,
     PRIMARY KEY(id_navire, name_nation),
     FOREIGN KEY(id_navire) REFERENCES navire(id_navire) ON DELETE CASCADE,
-    FOREIGN KEY(name) REFERENCES nation(name)
+    FOREIGN KEY(name) REFERENCES nation(name) ON DELETE CASCADE
 );
 
 CREATE TABLE diplomatics_relations(
     nation1 varchar(32) NOT NULL,
     nation2 varchar(32) NOT NULL,
-    CONSTRAINT CHK_nation1 FOREIGN KEY(nation1) REFERENCES nation(name),
-    CONSTRAINT CHK_nation2 FOREIGN KEY(nation2) REFERENCES nation(name),
-    CONSTRAINT CHK_nations CHECK(nation1 != nation2)
+    CONSTRAINT CHK_nation1 FOREIGN KEY(nation1) REFERENCES nation(name) ON DELETE CASCADE,
+    CONSTRAINT CHK_nation2 FOREIGN KEY(nation2) REFERENCES nation(name) ON DELETE CASCADE,
+    CONSTRAINT CHK_nations CHECK(nation1 != nation2),
 );
 
 CREATE TABLE cargo(
@@ -114,13 +114,19 @@ CREATE TABLE cargo(
     quantity int NOT NULL,
     volume int NOT NULL,
     cale_max int NOT NULL,
-    PRIMARY KEY(caragaison_id),
+    type type_voyage NOT NULL,
+    perishable boolean NOT NULL,
 
+    PRIMARY KEY(caragaison_id),
     FOREIGN KEY(id_navire) REFERENCES navire(id_navire) ON DELETE CASCADE,
     FOREIGN KEY(product_id) REFERENCES produits(product_id) ON DELETE CASCADE,
     FOREIGN KEY(quantity) REFERENCES produits(quantity),
     FOREIGN KEY(cale_max) REFERENCES navire(cale_max),
     FOREIGN KEY(volume) REFERENCES produits(volume),
+    FOREIGN KEY(type_voyage) REFERENCES voyage(type),
+    FOREIGN KEY(perishable) REFERENCES produits(perishable),
+
+    CONSTRAINT CHK_Produits  CHECK( (perishable == false AND type == 'continentale') OR (perishable == true AND type == 'intercontinental')),
     CONSTRAINT CHK_Cargaison CHECK(quantity * volume < cale_max)
 );
 
