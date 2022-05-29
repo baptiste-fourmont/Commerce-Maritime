176a6aae07d571dc2117714fc05f3ecc8c06d91c
Baptiste Fourmont <baptiste_fourmont@tutanota.com>
2022-05-29 15:15:52 +0200

    add req

--- REQ/req10.sql
+++ REQ/req10.sql
@@ -1,9 +1,9 @@
 /*
-    Les produits qui ont été acheté et vendu dans la même étape de même quantité
+    Les produits qui ont été acheté avec la même quantité
 */
 SELECT DISTINCT Buy_Product.product_id
 FROM Buy_Product
 FULL OUTER JOIN Sell_Product
-ON Buy_Product.id_etape = Sell_Product.id_etape AND Buy_Product.product_id = Sell_Product.product_id
+ON Buy_Product.id_etape = Sell_Product.id_etape
 WHERE Buy_Product.quantity = Sell_Product.quantity
 ORDER by Buy_Product.product_id;
