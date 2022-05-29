50df6e8e93b2495ee6ba65425869959e3811b6e3
Baptiste Fourmont <baptiste_fourmont@tutanota.com>
2022-05-29 16:31:19 +0200

    add req

---
+++ REQ/req15.sql
@@ -0,0 +1,4 @@
+/* Le produit qui a été vendu en plus grande quantité durant une étape */
+SELECT name, quantity
+FROM Sell_Product NATURAL JOIN produit
+WHERE quantity = (SELECT MAX(quantity) FROM Sell_Product)
