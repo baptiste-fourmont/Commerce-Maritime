d94b7ad36380688cd4b51ef24b4ee24098864331
Baptiste Fourmont <68250131+baptiste-fourmont@users.noreply.github.com>
2022-05-29 18:23:17 +0200

    Update req19.sql

---
+++ REQ/req19.sql
@@ -0,0 +1,12 @@
+WITH RECURSIVE Access(id_voyage, date_passage, passagers) AS
+(
+SELECT id_voyage, date_depart, passagers
+FROM voyage
+UNION
+SELECT etape_transitoire.id_voyage, etape_transitoire.date_passage, passagers + etape_transitoire.ascending_passagers - etape_transitoire.descending_passagers as passagers_new
+FROM etape_transitoire, Access
+NATURAL JOIN navire as n
+WHERE Access.id_voyage = etape_transitoire.id_voyage AND Access.date_passage < etape_transitoire.date_passage AND COALESCE(passagers,0) + COALESCE(etape_transitoire.ascending_passagers,0) - COALESCE(etape_transitoire.descending_passagers,0) >= 0 AND n.passagers_max >= COALESCE(passagers,0) + COALESCE(etape_transitoire.ascending_passagers,0) - COALESCE(etape_transitoire.descending_passagers,0)
+)
+SELECT id_voyage, date_passage, passagers FROM Access
+ORDER BY id_voyage;
