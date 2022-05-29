1eed5b889ce965d47606d2363c309a61ad4ffef8
Baptiste Fourmont <baptiste_fourmont@tutanota.com>
2022-05-29 15:34:46 +0200

    add req

---
+++ REQ/req11.sql
@@ -0,0 +1,12 @@
+/*  
+    Les etapes qui maximisent l’écart entre les passagers montées et descendus
+*/
+SELECT id_etape as numero_etape
+FROM etape_transitoire
+GROUP by id_etape
+HAVING MAX(ascending_passagers) - MIN(descending_passagers) >= 
+ALL ( 
+    SELECT MAX(ascending_passagers)-MIN(descending_passagers) 
+    FROM etape_transitoire 
+    GROUP by id_etape
+);
